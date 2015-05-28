6b1c2aa660e28ef1f09bd654b0097127391aa1da
julianbadillo <jbadillo@cern.ch>
2015-05-28 18:06:47 +0200

    separate plots

--- analyzeData.py
+++ analyzeData.py
@@ -11,14 +11,15 @@ import random
 
 from math import sqrt, ceil
 
-features = ['EventId','DER_mass_MMC','DER_mass_transverse_met_lep',
-  'DER_mass_vis','DER_pt_h','DER_deltaeta_jet_jet','DER_mass_jet_jet',
-  'DER_prodeta_jet_jet','DER_deltar_tau_lep','DER_pt_tot','DER_sum_pt',
-  'DER_pt_ratio_lep_tau','DER_met_phi_centrality','DER_lep_eta_centrality',
-  'PRI_tau_pt','PRI_tau_eta','PRI_tau_phi','PRI_lep_pt','RI_lep_eta,PRI_lep_phi',
-  'PRI_met','PRI_met_phi','PRI_met_sumet','PRI_jet_num','PRI_jet_leading_pt',
-  'PRI_jet_leading_eta','PRI_jet_leading_phi','PRI_jet_subleading_pt',
-  'PRI_jet_subleading_eta','PRI_jet_subleading_phi','PRI_jet_all_pt','Weight']
+features = ['DER_mass_MMC','DER_mass_transverse_met_lep','DER_mass_vis',
+'DER_pt_h','DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet',
+'DER_deltar_tau_lep','DER_pt_tot','DER_sum_pt','DER_pt_ratio_lep_tau',
+'DER_met_phi_centrality','DER_lep_eta_centrality','PRI_tau_pt',
+'PRI_tau_eta','PRI_tau_phi','PRI_lep_pt','PRI_lep_eta','PRI_lep_phi',
+'PRI_met','PRI_met_phi','PRI_met_sumet','PRI_jet_num','PRI_jet_leading_pt',
+'PRI_jet_leading_eta','PRI_jet_leading_phi','PRI_jet_subleading_pt',
+'PRI_jet_subleading_eta','PRI_jet_subleading_phi','PRI_jet_all_pt'
+]
 
 
 def plotDistribution(X):
@@ -30,6 +31,10 @@ def plotDistribution(X):
     c = 'rgbkmyc'
     m, n = X.shape
     for j in range(n):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.grid(True)
+        
         F = X[:,j] #only one feature
         step = (F.max() - F.min()) / slots
         x = np.linspace(F.min(), F.max(), slots)
@@ -41,11 +46,10 @@ def plotDistribution(X):
             i += 1
         #normalize
         y /= y.sum()
-        sub = plt.subplot(5,6,j+1)
-        sub.set_title(features[j])
-        s = sub.scatter( x, y, s=3, c=c[j%len(c)], marker='o', edgecolors='none')
+        plt.title(features[j])
+        s = plt.scatter( x, y, s=15, c=c[j%len(c)], marker='o', edgecolors='none')
     #plt.show()    
-    plt.savefig('distributions.png')
+        plt.savefig('distributions%s.png'%features[j])
 
 
 def convertLabel(l):
@@ -60,7 +64,7 @@ def main():
     data = np.loadtxt("training.csv", delimiter=',', skiprows=1,
             converters={32: convertLabel})
     #trim
-    data = data[:100,:]
+    #data = data[:100,:]
     print "Shape of the Train set", data.shape
     ids = data[:,0] #first column is id
     X_tr = data[:,1:31] #30 features
